1ad18ff50a9599962f0333ad906f1aff4f723264
MessaliHadj <lanymale33@gmail.com>
2024-10-16 14:50:07 +0200

    modified:   hwg-lafontn/src/pages/Home/Home.jsx

--- hwg-lafontn/src/pages/Home/Home.jsx
+++ hwg-lafontn/src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import Section1 from './Section1';
 import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
+import Section5 from './Section5';
 import "@/styles/HomeStyle.css";
 
 const Home = () => {
@@ -7,6 +10,9 @@ const Home = () => {
     <>
       <Section1 />
       <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
     </>
   )
 }
