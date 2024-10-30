7f56465d3d8f40353e5d17ec6db2794e09638d0e
MessaliHadj <lanymale33@gmail.com>
2024-10-30 11:43:30 +0100

    change env variable

--- hwg-api/db.config.js
+++ hwg-api/db.config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 let sequelize = new Sequelize(
-  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
